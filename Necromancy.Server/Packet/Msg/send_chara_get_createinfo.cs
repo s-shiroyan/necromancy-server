ae9a1a71df30785905433411b63ac6bf8499fcdf
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2019-06-14 22:56:21 +0800

    update

--- Necromancy.Server/Packet/Msg/send_chara_get_createinfo.cs
+++ Necromancy.Server/Packet/Msg/send_chara_get_createinfo.cs
@@ -13,11 +13,96 @@ namespace Necromancy.Server.Packet.Msg
 
         public override ushort Id => (ushort) MsgPacketId.send_chara_get_createinfo;
 
+        /// <summary>
+        /// [<= 8 array][Type unknown]
+        ///   [<= 8 array][Type unknown]
+        ///   [<= 8 array][Type unknown]
+        ///   [<= 5 array][Type unknown]
+        ///   [<= 10 array][Type unknown]
+        ///   [<= 140 array][Type unknown]
+        ///   [<= 14 array][Type unknown]
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="packet"></param>
         public override void Handle(NecClient client, NecPacket packet)
         {
             IBuffer res = BufferProvider.Provide();
             res.WriteInt32(0);
-            res.WriteInt32(0);
+
+            // 4bytes cmp < 8
+            res.WriteByte(1);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            //
+            res.WriteByte(0);
+
+            // 4bytes cmp < 8
+            res.WriteByte(1);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            //
+            res.WriteByte(0);
+
+            // 4bytes cmp < 5
+            res.WriteByte(1);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            //
+           
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);   
+            
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+     
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+        
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            
+            res.WriteByte(0);
+            res.WriteByte(0);
+
             Router.Send(client, (ushort) MsgPacketId.recv_chara_get_createinfo_r, res);
         }
     }
