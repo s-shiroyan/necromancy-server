fa3df236819760a662587b90a7a9e19f985694cc
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2019-06-21 15:50:44 +0800

    prepare base enter

---
+++ Necromancy.Server/Packet/Area/send_base_enter.cs
@@ -0,0 +1,23 @@
+using Necromancy.Server.Model;
+using Necromancy.Server.Packet.Id;
+
+namespace Necromancy.Server.Packet.Area
+{
+    public class send_base_enter : Handler
+    {
+        public send_base_enter(NecServer server) : base(server)
+        {
+        }
+
+        public override ushort Id => (ushort) AreaPacketId.send_base_enter;
+
+        public override void Handle(NecClient client, NecPacket packet)
+        {
+            // IBuffer res = BufferProvider.Provide();
+            // res.WriteInt32(0);
+
+
+            // Router.Send(client, (ushort) AreaPacketId.recv_base_check_version_r, res);
+        }
+    }
+}
--- Necromancy.Server/Packet/Id/AreaPacketId.cs
+++ Necromancy.Server/Packet/Id/AreaPacketId.cs
@@ -631,7 +631,7 @@ namespace Necromancy.Server.Packet.Id
     public enum AreaPacketId : ushort
     {
         send_base_check_version = 0x5705,
-        recv_base_check_version_r = 0xEFDD + 0x20  , //recv_base_check_version_r //  0x4E recv_random_box_get_item_all_r"
+        recv_base_check_version_r = 0xEFDD,
         send_base_enter = 0xAE43
     }
 }
