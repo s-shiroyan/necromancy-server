3f92681f62ada79a0d070efffc158a3750c5c280
WizOnHiraeth <51773103+WizOnHiraeth@users.noreply.github.com>
2019-07-24 22:22:12 -0500

    updated necserver

--- Necromancy.Server/NecServer.cs
+++ Necromancy.Server/NecServer.cs
@@ -171,9 +171,17 @@ namespace Necromancy.Server
             _areaConsumer.AddHandler(new send_chat_post_message(this));
             _areaConsumer.AddHandler(new send_chara_pose(this));
             _areaConsumer.AddHandler(new send_chara_pose_ladder(this));
-            _areaConsumer.AddHandler(new send_charabody_access_start(this));
-            _areaConsumer.AddHandler(new send_shortcut_request_regist(this));
-            _areaConsumer.AddHandler(new send_skill_start_cast(this));
+            _areaConsumer.AddHandler(new send_comment_set(this));
+            _areaConsumer.AddHandler(new send_comment_switch(this));
+            _areaConsumer.AddHandler(new send_refusallist_add_user(this));
+            _areaConsumer.AddHandler(new send_blacklist_open(this));
+            _areaConsumer.AddHandler(new send_emotion_update_type(this));
+            _areaConsumer.AddHandler(new send_blacklist_close(this));
+            _areaConsumer.AddHandler(new send_blacklist_clear(this));
+            _areaConsumer.AddHandler(new send_blacklist_lock(this));
+            _areaConsumer.AddHandler(new send_blacklist_unlock(this));
+            _areaConsumer.AddHandler(new send_wanted_entry(this));
+            _areaConsumer.AddHandler(new send_cmd_exec(this));
         }
     }
 }
