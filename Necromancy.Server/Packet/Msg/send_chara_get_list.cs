50d604635c0b7649f39a13e06cf7ee5ac1ee345b
Xeno <53803953+blackice36@users.noreply.github.com>
2019-09-02 21:19:26 -0500

    Added 13 Stringless recvs,
    Added variable arrays to Send_Chara_get_list.
    
    updated comments
    Updated Items quality int32s.

--- Necromancy.Server/Packet/Msg/send_chara_get_list.cs
+++ Necromancy.Server/Packet/Msg/send_chara_get_list.cs
@@ -36,10 +36,15 @@ namespace Necromancy.Server.Packet.Msg
             Router.Send(client, (ushort)MsgPacketId.recv_chara_notify_data_complete, res2);
         }
 
-        byte[] slot = new byte[] { 0, 1, }; // 2, 3, 4, };
+        byte[] slot = new byte[] {0,1,2,3};//3,4};
 
         int E = 0;
-
+        string[] MyCharacters = new string[] {"Zenkato","Test1","Xeno","Kadred","Ipa","Diablo",};
+        int[] MyRace = new int[]{0,4,3,3,0,2};
+        int[] MyGender = new int[]{1,1,0,0,1,1};
+        int[] MyWeaponType = new int[]{14,8,8,8,10,10};
+        byte[] MyHair = new byte[] { 5,0,0,0,5,5};
+        
 
 
         private void SendNotifyData(NecClient client)
@@ -49,29 +54,29 @@ namespace Necromancy.Server.Packet.Msg
             foreach (int y in slot)
             {
                 IBuffer res = BufferProvider.Provide();
-                res.WriteByte(1);//character slot, 0 for left, 1 for middle, 2 for right
-                res.WriteInt32(1);    //  Character ID
-                res.WriteFixedString("Character 1", 91); // 0x5B | 91x 1 byte
+                res.WriteByte(slot[E]);//character slot, 0 for left, 1 for middle, 2 for right
+                res.WriteInt32(E+256);    //  Character ID
+                res.WriteFixedString(MyCharacters[E], 91); // 0x5B | 91x 1 byte
 
                 res.WriteInt32(0); // 0 = Alive | 1 = Dead
                 res.WriteInt32(0);//character level stat
                 res.WriteInt32(0);//todo (unknown)
                 res.WriteInt32(0);//class stat 
                                   //
-                res.WriteInt32(1);//race flag
-                res.WriteInt32(1);//gender flag
-                res.WriteByte(0);//changing this byte makes hair and face change?
-                res.WriteByte(0);//hair color? i think
-                res.WriteByte(0);//changed nothing visibly
-                                 //
+                res.WriteInt32(MyRace[E]);//race flag
+                res.WriteInt32(MyGender[E]);//gender flag
+                res.WriteByte(0);//Hair Style
+                res.WriteByte(5);//Hair Color
+                res.WriteByte(0);//Face Style
+                                 
 
                 //ItemType Select See str_table SubID 121 for Item Type info. Increment by +1
                 int Armor = 25;         //Armor 25
                 int Accessory = 27;     //Accessory 26
                 int Shield = 21;        //Shield 19-21
-                int Weapon = 14;         //0 Knuckle, 1 Dagger, 3 1hSword, 7 1h axe (broken), 8 2hAxe, 9 spear, 10 blunt, 13 staff, 15 crossbow
+                //int Weapon = 14;         //0 Knuckle, 1 Dagger, 3 1hSword, 7 1h axe (broken), 8 2hAxe, 9 spear, 10 blunt, 13 staff, 15 crossbow
                                          //sub_483660 
-                res.WriteInt32(Weapon); //18	    				
+                res.WriteInt32(MyWeaponType[E]); //18	    				
                 res.WriteInt32(Shield); //17 	    		
                 res.WriteInt32(Armor); //16	        			
                 res.WriteInt32(Armor); //15	        				
@@ -92,40 +97,44 @@ namespace Necromancy.Server.Packet.Msg
                 res.WriteInt32(22);  //0 
 
 
-                //int[] EquipId = new int[] {15200601,15200601/*Shield* */,20000101/*Quiver*/,100101,200101,300101,400101,500101,690101/*Cape*/
-                //,30102401,30200103,30300101,30400112,70000201/*talkring*/,160801,260801,360801,460801,10800405/*Weapon*/ };
-
-
                 //sub_483420
                 int numEntries = 19;
 
                 int x = 0;
                 int[] EquipId = new int[19];
 
-                string CharacterSet = "";
+                string CharacterSet = MyCharacters[E];
 
                 switch (CharacterSet)
                 {
                     case "Xeno":
-                        EquipId = new int[] {10800405/*Weapon*/,15200702/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
-                    ,690101,690101,690101,261401/*Avatar Torso*/,561401/*Avatar Feet*/,461401/*Avatar Arms */,361401/*Avatar Legs*/,161401/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
+                        EquipId = new int[] {10800405/*Weapon*/,15100901/*Shield* */,210701/*Torso*/,110301/*head*/,360103/*legs*/,410505/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
+                    ,690101,690101,690101,210701/*Avatar Torso*/,560103/*Avatar Feet*/,410505/*Avatar Arms */,360103/*Avatar Legs*/,110301/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
                         break;
                     case "Kadred":
                         EquipId = new int[] {10800405/*Weapon*/,15100901/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
                     ,690101,690101,690101,260801/*Avatar Torso*/,560801/*Avatar Feet*/,460801/*Avatar Arms */,360801/*Avatar Legs*/,160801/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
                         break;
                     case "Zenkato":
-                        EquipId = new int[] {11400403/*Weapon*/,0/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,510301/*Feet*/,690101,690101/*Cape*/
+                        EquipId = new int[] {11400401/*Weapon*/,0/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,510301/*Feet*/,690101,690101/*Cape*/
                     ,690101,690101,690101,260801/*Avatar Torso*/,510301/*Avatar Feet*/,460801/*Avatar Arms */,360801/*Avatar Legs*/,100403/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
                         break;
                     case "Ipa":
-                        EquipId = new int[] {11300506/*Weapon*/,15100901/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
-                    ,690101,690101,690101,00252401/*Avatar Torso*/,560801/*Avatar Feet*/,460801/*Avatar Arms */,360801/*Avatar Legs*/,121901/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
-                        break;
+                     EquipId = new int[] {11300705/*Weapon*/,0/*Shield* */,260101/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
+                    ,690101,690101,690101,260104/*Avatar Torso*/,560801/*Avatar Feet*/,460801/*Avatar Arms */,361001/*Avatar Legs*/,160104/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
+                    break;
+                    case "Diablo":
+                     EquipId = new int[] {11400401/*Weapon*/,0/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
+                    ,690101,690101,690101,260701/*Avatar Torso*/,560801/*Avatar Feet*/,460801/*Avatar Arms */,360701/*Avatar Legs*/,160104/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
+                    break;
+                    case "Test1":
+                     EquipId = new int[] {10910405/*Weapon*/,15100801/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
+                    ,690101,690101,690101,261101/*Avatar Torso*/,561101/*Avatar Feet*/,461101/*Avatar Arms */,361101/*Avatar Legs*/,161101/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
+                    break;
                     default:
-                        EquipId = new int[] {10800405/*Weapon*/,15200702/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
-                    ,690101,690101,690101,261401/*Avatar Torso*/,561401/*Avatar Feet*/,461401/*Avatar Arms */,361401/*Avatar Legs*/,161401/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
-                        break;
+                     EquipId = new int[] {10800405/*Weapon*/,15100901/*Shield* */,260103/*Torso*/,110504/*head*/,360103/*legs*/,460103/*Arms*/,560103/*Feet*/,690101,690101/*Cape*/
+                    ,690101,690101,690101,260901/*Avatar Torso*/,560901/*Avatar Feet*/,460901/*Avatar Arms */,360901/*Avatar Legs*/,160901/*Avatar Head*/,690101,20000101/*Weapon Related*/ };
+                    break;
                 }
 
                 //sub_4948C0
@@ -144,12 +153,12 @@ namespace Necromancy.Server.Packet.Msg
                     res.WriteByte(0); //
                     x++;
 
-                    res.WriteByte(00);// Hair style from  chara\00\041\000\model  45 = this file C:\WO\Chara\chara\00\041\000\model\CM_00_041_11_045.nif
+                    res.WriteByte(MyHair[E]);// Hair style from  chara\00\041\000\model  45 = this file C:\WO\Chara\chara\00\041\000\model\CM_00_041_11_045.nif
                     res.WriteByte(00); //Face Style calls C:\Program Files (x86)\Steam\steamapps\common\Wizardry Online\data\chara\00\041\000\model\CM_00_041_10_010.nif.  must be 00 10, 20, 30, or 40 to work.
-                    res.WriteByte(0); // testing
-                    res.WriteByte(0); // testing
-                    res.WriteByte(0); // testing
-                    res.WriteByte(0); // testing
+                    res.WriteByte(0); // testing (Theory Torso Tex)
+                    res.WriteByte(0); // testing (Theory Pants Tex)
+                    res.WriteByte(0); // testing (Theory Hands Tex)
+                    res.WriteByte(0); // testing (Theory Feet Tex)
                     res.WriteByte(0); //Alternate texture for item model 
                     res.WriteByte(0); // seperate in assembly
 
@@ -178,30 +187,30 @@ namespace Necromancy.Server.Packet.Msg
                 res.WriteInt32(064); //Avatar Arms
                 res.WriteInt32(032); //Avatar Legs
                 res.WriteInt32(008); //Avatar Head  
-                res.WriteInt32(004); //???
-                res.WriteInt32(000); //Right Hand   
+                res.WriteInt32(004); //???Talk Ring
+                res.WriteInt32(000); //???Quiver   
 
 
                 //19x 4 byte //item quality(+#) or aura? 10 = +7, 19 = +6,(maybe just wep aura)
                 res.WriteInt32(10); //Right Hand    //1 for weapon
-                res.WriteInt32(002); //Left Hand     //2 for Shield
-                res.WriteInt32(016); //Torso         //16 for torso
-                res.WriteInt32(008); //Head          //08 for head
-                res.WriteInt32(032); //Legs          //32 for legs
-                res.WriteInt32(064); //Arms          //64 for Arms
-                res.WriteInt32(128); //Feet          //128 for feet
+                res.WriteInt32(10); //Left Hand     //2 for Shield
+                res.WriteInt32(10); //Torso         //16 for torso
+                res.WriteInt32(10); //Head          //08 for head
+                res.WriteInt32(10); //Legs          //32 for legs
+                res.WriteInt32(10); //Arms          //64 for Arms
+                res.WriteInt32(10); //Feet          //128 for feet
                 res.WriteInt32(004); //???Cape
                 res.WriteInt32(rr); //???Ring
                 res.WriteInt32(rr); //???Earring
                 res.WriteInt32(rr); //???Necklace
                 res.WriteInt32(rr); //???Belt
-                res.WriteInt32(016); //Avatar Torso
-                res.WriteInt32(128); //Avatar Feet
-                res.WriteInt32(064); //Avatar Arms
-                res.WriteInt32(032); //Avatar Legs
-                res.WriteInt32(008); //Avatar Head  
-                res.WriteInt32(004); //???
-                res.WriteInt32(000); //Right Hand   
+                res.WriteInt32(10); //Avatar Torso
+                res.WriteInt32(10); //Avatar Feet
+                res.WriteInt32(10); //Avatar Arms
+                res.WriteInt32(10); //Avatar Legs
+                res.WriteInt32(10); //Avatar Head  
+                res.WriteInt32(10); //???Talk Ring
+                res.WriteInt32(00); //???Quiver    
 
 
                 res.WriteByte(19);//changed nothing visibly
