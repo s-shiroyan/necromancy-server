4648e1d0f0c87b5cc55f2cfd7c7c74472b4a4d89
WizOnHiraeth <51773103+WizOnHiraeth@users.noreply.github.com>
2019-09-29 20:45:30 -0500

    added work in progress logout

--- Necromancy.Server/Packet/Area/send_chat_post_message.cs
+++ Necromancy.Server/Packet/Area/send_chat_post_message.cs
@@ -718,29 +718,40 @@ namespace Necromancy.Server.Packet.Area
 
         private void LogOut(NecClient client)
         {
-            byte[] byteArr = new byte[20] { 0x00, 0x12, 0xDD, 0xEF, 0, 0, 0, 0, 0, 0, 0, 0,
-                0 ,0 ,0 ,0 ,0, 0, 0, 0};
+            byte[] byteArr = new byte[8] { 0x00, 0x06, 0xEE, 0x91, 0, 0, 0, 0 };
 
             IBuffer res = BufferProvider.Provide();
 
-
             res.WriteInt32(0);
-            res.WriteByte(0);
-            res.WriteInt32(0xE1F50501);
-            res.WriteByte(0);
-            res.WriteByte(0);
-            res.WriteByte(0);
-            res.WriteInt32(0x25E6852C);
-
 
             res.SetPositionStart();
 
-            for (int i = 4; i < 20; i++)
+            for (int i = 4; i < 8; i++)
             {
                 byteArr[i] += res.ReadByte();
             }
 
             client.Session.msgSocket.Send(byteArr);
+
+            System.Threading.Thread.Sleep(4000);
+
+            byte[] byteArrr = new byte[9] { 0x00, 0x07, 0x52, 0x56, 0, 0, 0, 0, 0 };
+
+            IBuffer res2 = BufferProvider.Provide();
+
+            res2.WriteInt32(0);
+            res2.WriteByte(0);
+
+            res2.SetPositionStart();
+
+            for (int i = 4; i < 9; i++)
+            {
+                byteArrr[i] += res2.ReadByte();
+            }
+
+            client.Session.msgSocket.Send(byteArrr);
+
+
         }
         private void AdminConsoleSelectPackageUpdate(NecClient client)
         {
