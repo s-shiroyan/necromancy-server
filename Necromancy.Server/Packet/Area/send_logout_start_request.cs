05d4cb12b20a4260a15c7ad3de31f1467c422300
WizOnHiraeth <51773103+WizOnHiraeth@users.noreply.github.com>
2019-10-01 21:46:11 -0500

    Tested&FinalizedCharaMake
    also added, fix for 5 slots and small addition too logout

--- Necromancy.Server/Packet/Area/send_logout_start_request.cs
+++ Necromancy.Server/Packet/Area/send_logout_start_request.cs
@@ -43,6 +43,7 @@ namespace Necromancy.Server.Packet.Area
             IBuffer res2 = BufferProvider.Provide();
             IBuffer res3 = BufferProvider.Provide();
             IBuffer res4 = BufferProvider.Provide();
+            IBuffer res5 = BufferProvider.Provide();
 
             if (client.Character.logoutCanceled == 0)
             {
