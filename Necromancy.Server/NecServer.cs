40eee73f414cb6833b599dd8eec3f7d393a24e98
Xeno <53803953+blackice36@users.noreply.github.com>
2019-10-12 10:00:46 -0500

    Bug Fix - fixed crash on keyboard mapping change

--- Necromancy.Server/NecServer.cs
+++ Necromancy.Server/NecServer.cs
@@ -273,7 +273,7 @@ namespace Necromancy.Server
             //_areaConsumer.AddHandler(new send_quest_get_story_quest_works(this));
             //_areaConsumer.AddHandler(new send_shortcut_request_data(this));
             //_areaConsumer.AddHandler(new send_skill_request_info(this));
-            //_areaConsumer.AddHandler(new send_sv_conf_option_change(this));
+            _areaConsumer.AddHandler(new send_sv_conf_option_change(this));
 
         }
     }
