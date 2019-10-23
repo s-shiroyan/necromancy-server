a3dc9f77c8cc65d4baebed3a36be6ba9650f1d10
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2019-10-23 08:41:11 +0800

    use map from server, format code

--- Necromancy.Server/Packet/Msg/send_chara_select.cs
+++ Necromancy.Server/Packet/Msg/send_chara_select.cs
@@ -1,9 +1,7 @@
 using Arrowgene.Services.Buffers;
 using Necromancy.Server.Common;
 using Necromancy.Server.Model;
-using Necromancy.Server.Setting;
 using Necromancy.Server.Packet.Id;
-using System;
 
 namespace Necromancy.Server.Packet.Msg
 {
@@ -11,7 +9,6 @@ namespace Necromancy.Server.Packet.Msg
     {
         public send_chara_select(NecServer server) : base(server)
         {
-
         }
 
         public override ushort Id => (ushort) MsgPacketId.send_chara_select;
@@ -26,69 +23,72 @@ namespace Necromancy.Server.Packet.Msg
             {
                 foreach (Character myCharacter in Database.SelectCharacterBySoulId(client.Character.SoulId))
                 {
-                    Logger.Debug(client, $"CharacterSlotId: {myCharacter.Id} Comparing to CharacterIdInSelectedSlot: {CharacterIdInSelectedSlot}");
+                    Logger.Debug(client,
+                        $"CharacterSlotId: {myCharacter.Id} Comparing to CharacterIdInSelectedSlot: {CharacterIdInSelectedSlot}");
                     if (myCharacter.Id == CharacterIdInSelectedSlot)
                     {
                         client.Character = myCharacter;
-                        Logger.Debug(client, $"Found a Match! myCharacter.Id: {myCharacter.Id} is equal to CharacterIdInSelectedSlot: {CharacterIdInSelectedSlot}");
+                        Logger.Debug(client,
+                            $"Found a Match! myCharacter.Id: {myCharacter.Id} is equal to CharacterIdInSelectedSlot: {CharacterIdInSelectedSlot}");
                         break;
                     }
                 }
             }
 
-                //Settings for Map Entry until settings are databased.
-                client.Character.MapId = 1001007;//Set your map here. See reference table in "MapSettings.CS"
+            //Settings for Map Entry until settings are databased.
+            client.Character.MapId = 1001007; //Set your map here. See reference table in "MapSettings.CS"
 
-                if (client.Character.NewCharaProtocol == true) { client.Character.MapId = 1001902; } // special over-ride for new characters 1st time login after character creation (yes it has its own dedicated map)
-                
-                
-                // TODO verify new Map Setting Method
-                Map map = Server.Map.Get(client.Character.MapId);
-                client.Character.X = map.X;
-                client.Character.Y = map.Y;
-                client.Character.Z = map.Z;
-                //client.Character.viewOffset = byte.Parse(theMapSettings[9]);
-                break;
+            if (client.Character.NewCharaProtocol == true)
+            {
+                client.Character.MapId = 1001902;
+            } // special over-ride for new characters 1st time login after character creation (yes it has its own dedicated map)
 
-                /*Outdated Method to be removed post test
-                string[] theMapSettings = MapSetting.MapLoadInfo(client.Character.MapId);
-                client.Character.X = float.Parse(theMapSettings[6]);
-                client.Character.Y = float.Parse(theMapSettings[7]);
-                client.Character.Z = float.Parse(theMapSettings[8]);
-                //client.Character.viewOffset = byte.Parse(theMapSettings[9]);
-                */
+
+            // TODO verify new Map Setting Method
+            Map map = Server.Map.Get(client.Character.MapId);
+            client.Character.X = map.X;
+            client.Character.Y = map.Y;
+            client.Character.Z = map.Z;
+            client.Character.viewOffset = (byte) map.Orientation;
+
+            /*Outdated Method to be removed post test
+            string[] theMapSettings = MapSetting.MapLoadInfo(client.Character.MapId);
+            client.Character.X = float.Parse(theMapSettings[6]);
+            client.Character.Y = float.Parse(theMapSettings[7]);
+            client.Character.Z = float.Parse(theMapSettings[8]);
+            //client.Character.viewOffset = byte.Parse(theMapSettings[9]);
+            */
 
             IBuffer res2 = BufferProvider.Provide();
 
-                res2.WriteInt32(0); // error check
-                res2.WriteInt32(0); // error check
+            res2.WriteInt32(0); // error check
+            res2.WriteInt32(0); // error check
 
-                //sub_494c50
-                res2.WriteInt32(128);
-                res2.WriteInt32(2);
-                res2.WriteInt32(88888888);
-                res2.WriteInt16(4);
-                //sub_4834C0
-                res2.WriteByte(69);
+            //sub_494c50
+            res2.WriteInt32(128);
+            res2.WriteInt32(2);
+            res2.WriteInt32(88888888);
+            res2.WriteInt16(4);
+            //sub_4834C0
+            res2.WriteByte(69);
 
-                //sub_494B90 - for loop
-                for (int i = 0; i < 0x80; i++)
-                {
-                    res2.WriteInt32(i);
-                    res2.WriteFixedString($"Channel {i}", 97);
-                    res2.WriteByte(1);   //bool 1 | 0
-                    res2.WriteInt16(0xFFFF);  //Max players
-                    res2.WriteInt16(0xFF);  //Current players
-                    res2.WriteByte(0);
-                    res2.WriteByte(0);
-                    //
-                }
+            //sub_494B90 - for loop
+            for (int i = 0; i < 0x80; i++)
+            {
+                res2.WriteInt32(i);
+                res2.WriteFixedString($"Channel {i}", 97);
+                res2.WriteByte(1); //bool 1 | 0
+                res2.WriteInt16(0xFFFF); //Max players
+                res2.WriteInt16(0xFF); //Current players
+                res2.WriteByte(0);
+                res2.WriteByte(0);
+                //
+            }
 
 
-                res2.WriteByte(10); //# of channels
+            res2.WriteByte(10); //# of channels
 
-                Router.Send(client, (ushort)MsgPacketId.recv_chara_select_channel_r, res2, ServerType.Msg);
-            
+            Router.Send(client, (ushort) MsgPacketId.recv_chara_select_channel_r, res2, ServerType.Msg);
         }
     }
 }
