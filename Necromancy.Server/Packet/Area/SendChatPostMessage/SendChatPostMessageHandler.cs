6b31de15bf65c1f4f825edef9e88db54e77e5d15
Xeno <53803953+blackice36@users.noreply.github.com>
2019-10-31 21:17:39 -0500

    Remembered to hit save Button... adding to Commit
    Chat changes to make Change Form test command work.   prep work for event handlers

--- Necromancy.Server/Packet/Area/SendChatPostMessage/SendChatPostMessageHandler.cs
+++ Necromancy.Server/Packet/Area/SendChatPostMessage/SendChatPostMessageHandler.cs
@@ -588,40 +588,37 @@ namespace Necromancy.Server.Packet.Area.SendChatPostMessage
 
         private void ChangeFormMenu(NecClient client)
         {
+            if (client.Character.selectExecCode == -1)
+            {
+                IBuffer res2 = BufferProvider.Provide();
+                res2.WriteInt32(0); //1 = cinematic, 0 Just start the event without cinematic
+                res2.WriteByte(0);
 
-            IBuffer res2 = BufferProvider.Provide();
-            res2.WriteInt32(0); //1 = cinematic, 0 Just start the event without cinematic
-            res2.WriteByte(0);
 
-            Router.Send(client, (ushort)AreaPacketId.recv_event_start, res2, ServerType.Area);
-
-
+                Router.Send(client, (ushort)AreaPacketId.recv_event_start, res2, ServerType.Area);
 
                 IBuffer res3 = BufferProvider.Provide();
                 res3.WriteCString("Male"); //Length 0x601
                 Router.Send(client, (ushort)AreaPacketId.recv_event_select_push, res3, ServerType.Area);   // It's the first choice
 
-
-
-
                 IBuffer res70 = BufferProvider.Provide();
-                    res70.WriteCString("Female"); //Length 0x601
-                    Router.Send(client, (ushort)AreaPacketId.recv_event_select_push, res70, ServerType.Area);   // It's the second choice
+                res70.WriteCString("Female"); //Length 0x601
+                Router.Send(client, (ushort)AreaPacketId.recv_event_select_push, res70, ServerType.Area);   // It's the second choice
 
-      
-  
 
-            IBuffer res1 = BufferProvider.Provide();
 
-            res1.WriteCString("Character appareance change \n Price : 9000 Pieces"); // It's the title dude
-            res1.WriteInt32(0xFFFD); // This is the Event Type.  0xFFFD sends a 58 byte packet
-            Router.Send(client, (ushort)AreaPacketId.recv_event_select_exec, res1, ServerType.Area);
+                IBuffer res1 = BufferProvider.Provide();
+
+                res1.WriteCString("Character appareance change \n Price : 9000 Pieces"); // It's the title dude
+                res1.WriteInt32(6); // This is the Event Type.  0xFFFD sends a 58 byte packet
+                Router.Send(client, (ushort)AreaPacketId.recv_event_select_exec, res1, ServerType.Area);
+            }
 
             if (client.Character.selectExecCode != -1)
             { 
                 IBuffer res = BufferProvider.Provide();
                 res.WriteInt32(client.Character.Raceid); // race
-                res.WriteInt32(client.Character.selectExecCode+1); // gender 1 = female, 2 = male
+                res.WriteInt32(client.Character.selectExecCode); // gender 0 = female, 1 = male
                 res.WriteByte(client.Character.HairId); ; //hair
                 res.WriteByte(client.Character.HairColorId); //color
                 res.WriteByte(client.Character.FaceId); //face
