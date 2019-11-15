df1c3115ad240569908eb3bf8f456b83ec6e0b8b
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2019-11-15 14:12:21 +0800

    update

--- release.sh
+++ release.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish?tabs=netcore2x
-printenv
 RootFolder="${RELEASE_ROOT_FOLDER:-./}"
 read -r VERSION<$RootFolder/necromancy.version
 mkdir ./release
-echo Version: $VERSION
-ls
+echo Building Version: $VERSION
 for RUNTIME in win-x86 win-x64 linux-x64 osx-x64; do
     # Server
     dotnet publish Necromancy.Cli/Necromancy.Cli.csproj /p:Version=$VERSION /p:FromMSBuild=true --runtime $RUNTIME --configuration Release --output ../publish/$RUNTIME-$VERSION/Server
@@ -13,5 +11,4 @@ for RUNTIME in win-x86 win-x64 linux-x64 osx-x64; do
     cp -r ./ReleaseFiles/. ./publish/$RUNTIME-$VERSION/
     # Pack
     tar cjf ./release/$RUNTIME-$VERSION.tar.gz ./publish/$RUNTIME-$VERSION
-done 
- ls
+done
