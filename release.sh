2cd2587d20938b82f4eea8a18a0ddce58db5678d
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2019-11-15 14:34:03 +0800

    upload release

--- release.sh
+++ release.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 # https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish?tabs=netcore2x
 RootFolder="${RELEASE_ROOT_FOLDER:-./}"
+# Read version
 read -r VERSION<$RootFolder/necromancy.version
-mkdir ./release
+# GitHub Action Environment Variable (https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions#set-an-environment-variable-set-env)
+echo "::set-env name=NEC_VERSION::$VERSION"
+#
 echo Building Version: $VERSION
+mkdir ./release
 for RUNTIME in win-x86 win-x64 linux-x64 osx-x64; do
     # Server
     dotnet publish Necromancy.Cli/Necromancy.Cli.csproj /p:Version=$VERSION /p:FromMSBuild=true --runtime $RUNTIME --configuration Release --output ./publish/$RUNTIME-$VERSION/Server
@@ -12,4 +16,3 @@ for RUNTIME in win-x86 win-x64 linux-x64 osx-x64; do
     # Pack
     tar cjf ./release/$RUNTIME-$VERSION.tar.gz ./publish/$RUNTIME-$VERSION
 done
-ls
