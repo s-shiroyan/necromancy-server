59e21b6184ee41668e48463686a86886ed0c948e
Xeno <53803953+blackice36@users.noreply.github.com>
2019-12-26 06:23:58 -0600

    Restore Death and Player Damage Post Merge
    ToDo:  timings are off on damage notifications.
    Variables for player death need tweaking

---
+++ Necromancy.Server/Packet/Response/RecvCharaUpdateHp.cs
@@ -0,0 +1,27 @@
+using Arrowgene.Services.Buffers;
+using Necromancy.Server.Chat;
+using Necromancy.Server.Common;
+using Necromancy.Server.Model;
+using Necromancy.Server.Packet.Id;
+
+namespace Necromancy.Server.Packet.Response
+{
+    public class RecvCharaUpdateHp : PacketResponse
+    {
+        private int _currentHp;
+        public RecvCharaUpdateHp(int currentHp)
+            : base((ushort)AreaPacketId.recv_chara_update_hp, ServerType.Area)
+        {
+            _currentHp = currentHp;
+        }
+
+        protected override IBuffer ToBuffer()
+        {
+            if (_currentHp < 0) { _currentHp = 0; }
+
+            IBuffer res7 = BufferProvider.Provide();
+            res7.WriteInt32(_currentHp);
+            return res7;
+        }
+    }
+}
