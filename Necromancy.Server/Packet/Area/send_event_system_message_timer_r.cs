248099517fe8da36a834decd2e80813a1c5b887c
Unknownone69 <31863801+Unknownone69@users.noreply.github.com>
2019-12-30 20:41:54 -0800

    Added send_event_system_message_timer_r.cs

---
+++ Necromancy.Server/Packet/Area/send_event_system_message_timer_r.cs
@@ -0,0 +1,26 @@
+using Arrowgene.Services.Buffers;
+using Necromancy.Server.Common;
+using Necromancy.Server.Model;
+using Necromancy.Server.Packet.Id;
+using System;
+
+namespace Necromancy.Server.Packet.Area
+{
+    public class send_event_system_message_timer_r : ClientHandler
+    {
+        public send_event_system_message_timer_r(NecServer server) : base(server)
+        {
+        }
+
+
+        public override ushort Id => (ushort)AreaPacketId.send_event_system_message_timer_r;
+
+        public override void Handle(NecClient client, NecPacket packet)
+        {
+            IBuffer res = BufferProvider.Provide();
+            res.WriteCString("ToBeFound");
+            Router.Send(client.Map, (ushort)AreaPacketId.recv_event_system_message_timer, res, ServerType.Area);
+        }
+
+    }
+}
