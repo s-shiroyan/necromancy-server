9b6e47c6b51bd86483bd1e47a57a76495c34f53b
Unknownone69 <31863801+Unknownone69@users.noreply.github.com>
2020-02-07 01:38:43 -0800

    unions are stubborn, added string_names.xls

--- Necromancy.Server/Packet/Area/send_event_access_object.cs
+++ Necromancy.Server/Packet/Area/send_event_access_object.cs
@@ -497,11 +497,11 @@ namespace Necromancy.Server.Packet.Area
         {
             IBuffer res2 = BufferProvider.Provide();
             res2.WriteInt32(0);
-            Router.Send(client, (ushort)AreaPacketId.recv_union_request_establish_r, res, ServerType.Area);
+            //Router.Send(client, (ushort)AreaPacketId.recv_union_request_establish_r, res2, ServerType.Area);
             IBuffer res = BufferProvider.Provide();
             //recv_union_open_window = 0x7D75,
             //no structure
-            //Router.Send(client, (ushort)AreaPacketId.recv_union_open_window, res, ServerType.Area);
+            Router.Send(client, (ushort)AreaPacketId.recv_union_open_window, res, ServerType.Area);
         }
         private void SpareEventParts(NecClient client, NpcSpawn npcSpawn)
         {
--- Necromancy.Server/Packet/Area/send_union_close_window.cs
+++ Necromancy.Server/Packet/Area/send_union_close_window.cs
@@ -22,8 +22,32 @@ namespace Necromancy.Server.Packet.Area
             Router.Send(client, (ushort) AreaPacketId.recv_union_close_window_r, res, ServerType.Area);
 
             IBuffer res2 = BufferProvider.Provide();
-            //res2.WriteInt32(0);
-            Router.Send(client, (ushort)AreaPacketId.recv_event_sync, res2, ServerType.Area);
+            uint cid = client.Character.InstanceId;
+            res2.WriteInt32(cid);
+            res2.WriteFixedString("unk", 0x31); //size is 0x31
+            res2.WriteInt32(cid);
+            res2.WriteInt32(cid);
+            res2.WriteInt32(cid);
+            res2.WriteInt32(cid);
+            res2.WriteInt32(cid);
+            res2.WriteInt32(cid);
+            res2.WriteInt32(cid);
+            res2.WriteByte(1);
+            res2.WriteInt32(cid);
+            res2.WriteInt32(cid);
+            res2.WriteByte(2);
+            res2.WriteByte(3);
+            res2.WriteInt32(cid);
+            res2.WriteInt16(69);
+            res2.WriteFixedString("union", 0x196); //size is 0x196
+            for (int i = 0; i < 8; i++)
+                res2.WriteInt32(cid);
+            res2.WriteByte(4);
+            Router.Send(client.Map, (ushort)MsgPacketId.recv_union_notify_detail, res2, ServerType.Msg);
+
+            IBuffer res3 = BufferProvider.Provide();
+            //res3.WriteInt32(0);
+            Router.Send(client, (ushort)AreaPacketId.recv_event_sync, res3, ServerType.Area);
         }
 
     }
