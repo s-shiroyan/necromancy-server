5381b4e70438f27291ba38f6c93921072076e7ff
Unknownone69 <31863801+Unknownone69@users.noreply.github.com>
2020-05-19 06:20:57 -0700

    Update PlayersCommand.cs
    Removed server.Clients.Remove(theirClient); and changed the way we check to see if we should display the characters.

--- Necromancy.Server/Chat/Command/Commands/PlayersCommand.cs
+++ Necromancy.Server/Chat/Command/Commands/PlayersCommand.cs
@@ -42,14 +42,10 @@ namespace Necromancy.Server.Chat.Command.Commands
                 case "world": //tells you all the people in the world
                     foreach (NecClient theirClient in Server.Clients.GetAll())
                     {
-                        if (theirClient.Map.Id != -1)
+                        if (theirClient.AreaConnection != null)
                         {
                             responses.Add(ChatResponse.CommandError(client,
                             $"{theirClient.Character.Name} {theirClient.Soul.Name} is on Map {theirClient.Character.MapId} with InstanceID {theirClient.Character.InstanceId}"));
-                        }
-                        else
-                        {
-                            Server.Clients.Remove(theirClient);
                         }
                     }
 
