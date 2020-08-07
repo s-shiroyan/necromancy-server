a2f0bbe79e69b99a758fdc7a954573b9aea98253
Raezaiel <istari40@hotmail.com>
2020-08-06 21:14:16 -0400

    fixed skillbar in wrong slot

--- Necromancy.Server/Packet/Area/send_shortcut_request_data.cs
+++ Necromancy.Server/Packet/Area/send_shortcut_request_data.cs
@@ -22,14 +22,14 @@ namespace Necromancy.Server.Packet.Area
             // Need to find definitions for different action types as Ids do overlap,  0 ?, 1 ?, 2 ? , 3 for skill_tree item, 4 for system , 5 for emote
             // Should we have a shortcutBarItem class?
             const int MAX_SHORTCUT_BARS = 5;
-            for (int i = 1; i <= MAX_SHORTCUT_BARS; i++)
+            for (int i = 0; i < MAX_SHORTCUT_BARS; i++)
             {
                 ShortcutBar shortcutBar = Database.GetShortcutBar(client.Character, i);
                 for (int j = 0; j < ShortcutBar.COUNT; j++)
                 {
                     if (shortcutBar.Item[j] is null) continue;
                     IBuffer res0 = BufferProvider.Provide();
-                    res0.WriteByte(0);
+                    res0.WriteByte((byte) i);
                     res0.WriteByte((byte) j);
                     res0.WriteInt32((int) shortcutBar.Item[j].Type);
                     res0.WriteInt64(shortcutBar.Item[j].Id);       // SkillId from skill_tree.csv for class skills
