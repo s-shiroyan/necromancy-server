fec1e97f07147357a4d13a09f792cb060e96c7a9
Unknownone69 <31863801+Unknownone69@users.noreply.github.com>
2020-08-10 12:56:42 -0700

    Fixed file inconsistiencies across merges

--- Necromancy.Server/Packet/Area/send_soul_dispitem_request_data.cs
+++ Necromancy.Server/Packet/Area/send_soul_dispitem_request_data.cs
@@ -4,7 +4,7 @@ using Necromancy.Server.Data.Setting;
 using Necromancy.Server.Model;
 using Necromancy.Server.Model.ItemModel;
 using Necromancy.Server.Packet.Id;
-using Necromancy.Server.Packet.Receive;
+using Necromancy.Server.Packet.Receive.Area;
 using System.Collections.Generic;
 
 namespace Necromancy.Server.Packet.Area
