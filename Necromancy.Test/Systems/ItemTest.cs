afed940ee93f6f83c32eb23914e01efe2249583f
Raezaiel <istari40@hotmail.com>
2020-08-24 01:02:37 -0400

    added first test methods

---
+++ Necromancy.Test/Systems/ItemTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Necromancy.Test.Systems
+{
+    [TestClass]
+    class ItemTest
+    {
+
+        [TestMethod]
+        public void TestSpawnItemNoItemFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestGetSpawnItemNoSpawnFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestGetBaseItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestGetBaseItemNoItemFound()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
