207580f709307a6f43ec2133763dfd96acb17454
Xeno <53803953+blackice36@users.noreply.github.com>
2020-10-01 12:21:51 -0500

    recv_event_quest_list_begin
    It took me 4 hours over 3 days :'-(

--- Necromancy.Server/Chat/Command/Commands/NoStringTestCommand.cs
+++ Necromancy.Server/Chat/Command/Commands/NoStringTestCommand.cs
@@ -26,12 +26,86 @@ namespace Necromancy.Server.Chat.Command.Commands
             }
 
             IBuffer res = BufferProvider.Provide();
+            int numEntries = 0x2;
 
-                res = BufferProvider.Provide();
-            //res.WriteInt64(Id);
-            res.WriteUInt32(0); // Current durability points
-            res.WriteUInt32(0); // Current durability points
-            Router.Send(client, (ushort)0x11EE, res, ServerType.Area);
+            res.WriteInt32(0);
+            res.WriteInt32(numEntries); //less than 0x1E
+
+            if (numEntries == 0)
+            {
+                res.WriteInt32(0);
+            }
+            else
+            {
+                //sub_4936D0
+                res.WriteInt32(0);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteFixedString("Test", 0x61);
+                res.WriteInt32(0);
+                res.WriteInt32(0);
+                res.WriteFixedString("SecondTest", 0x61);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteInt32(0);
+                res.WriteInt32(0);
+
+                res.WriteInt32(0);
+                res.WriteInt32(0);
+                res.WriteInt32(0);
+
+                res.WriteInt32(0);
+                res.WriteInt32(0);
+                res.WriteInt32(0);
+
+                for (int i = 0; i < 0xA; i++)
+                {
+                    //sub 493640
+                    res.WriteInt32(0);
+                    res.WriteFixedString("", 0x10);
+                    res.WriteInt16(0);
+                    res.WriteInt32(0);
+                    res.WriteByte(0);
+                    res.WriteInt16(0);
+                }
+
+                res.WriteByte(0);
+
+                for (int i = 0; i < 0xC; i++)
+                {
+                    //sub 493640
+                    res.WriteInt32(0);
+                    res.WriteFixedString("", 0x10);
+                    res.WriteInt16(0);
+                    res.WriteInt32(0);
+                    res.WriteByte(0);
+                    res.WriteInt16(0);
+                }
+
+                res.WriteByte(0);
+
+                res.WriteInt32(0);
+                //Ret
+
+                res.WriteFixedString("", 0x181);
+                res.WriteFixedString("", 0x181);
+                for (int i = 0; i < 0x5; i++)
+                {
+                    res.WriteByte(0);
+                    res.WriteInt32(0);
+                    res.WriteInt32(0);
+                    res.WriteInt32(0);
+                    res.WriteInt32(0);
+                }
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteInt16(0);
+                res.WriteInt16(0);
+                res.WriteInt32(0);
+                //Ret
+                res.WriteInt32(0);
+            }
+            Router.Send(client, (ushort)0x1ACA, res, ServerType.Area);
 
 
         }
