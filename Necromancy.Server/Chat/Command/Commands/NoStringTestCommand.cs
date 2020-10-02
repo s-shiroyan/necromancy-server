a32bec079c8ae201fc404ad0d6b00b6fd8ecd9eb
Xeno <53803953+blackice36@users.noreply.github.com>
2020-10-02 09:22:41 -0500

    New Area Structures 20 of 301

--- Necromancy.Server/Chat/Command/Commands/NoStringTestCommand.cs
+++ Necromancy.Server/Chat/Command/Commands/NoStringTestCommand.cs
@@ -28,84 +28,9 @@ namespace Necromancy.Server.Chat.Command.Commands
             IBuffer res = BufferProvider.Provide();
             int numEntries = 0x2;
 
-            res.WriteInt32(0);
-            res.WriteInt32(numEntries); //less than 0x1E
+            //res.WriteInt32(0);
 
-            if (numEntries == 0)
-            {
-                res.WriteInt32(0);
-            }
-            else
-            {
-                //sub_4936D0
-                res.WriteInt32(0);
-                res.WriteByte(0);
-                res.WriteByte(0);
-                res.WriteFixedString("Test", 0x61);
-                res.WriteInt32(0);
-                res.WriteInt32(0);
-                res.WriteFixedString("SecondTest", 0x61);
-                res.WriteByte(0);
-                res.WriteByte(0);
-                res.WriteInt32(0);
-                res.WriteInt32(0);
-
-                res.WriteInt32(0);
-                res.WriteInt32(0);
-                res.WriteInt32(0);
-
-                res.WriteInt32(0);
-                res.WriteInt32(0);
-                res.WriteInt32(0);
-
-                for (int i = 0; i < 0xA; i++)
-                {
-                    //sub 493640
-                    res.WriteInt32(0);
-                    res.WriteFixedString("", 0x10);
-                    res.WriteInt16(0);
-                    res.WriteInt32(0);
-                    res.WriteByte(0);
-                    res.WriteInt16(0);
-                }
-
-                res.WriteByte(0);
-
-                for (int i = 0; i < 0xC; i++)
-                {
-                    //sub 493640
-                    res.WriteInt32(0);
-                    res.WriteFixedString("", 0x10);
-                    res.WriteInt16(0);
-                    res.WriteInt32(0);
-                    res.WriteByte(0);
-                    res.WriteInt16(0);
-                }
-
-                res.WriteByte(0);
-
-                res.WriteInt32(0);
-                //Ret
-
-                res.WriteFixedString("", 0x181);
-                res.WriteFixedString("", 0x181);
-                for (int i = 0; i < 0x5; i++)
-                {
-                    res.WriteByte(0);
-                    res.WriteInt32(0);
-                    res.WriteInt32(0);
-                    res.WriteInt32(0);
-                    res.WriteInt32(0);
-                }
-                res.WriteByte(0);
-                res.WriteByte(0);
-                res.WriteInt16(0);
-                res.WriteInt16(0);
-                res.WriteInt32(0);
-                //Ret
-                res.WriteInt32(0);
-            }
-            Router.Send(client, (ushort)0x1ACA, res, ServerType.Area);
+            Router.Send(client, (ushort)0x1B22, res, ServerType.Area);
 
 
         }
