909a9a2cc6300e04fa4b2f8c47325a9f4d134d03
Raezaiel <istari40@hotmail.com>
2020-12-30 16:27:08 -0500

    updatedecrpyt

--- Necromancy.Server/Chat/Command/Commands/ItemInstanceCommand.cs
+++ Necromancy.Server/Chat/Command/Commands/ItemInstanceCommand.cs
@@ -31,27 +31,28 @@ namespace Necromancy.Server.Chat.Command.Commands
         public override void Execute(string[] command, NecClient client, ChatMessage message,
             List<ChatResponse> responses)
         {
-
+            const int NUMBER = 20;
+            for(int i = 0; i < NUMBER; i++) { 
             IBuffer res = BufferProvider.Provide();
-            res.WriteUInt64(10001);      //SPAWN ID
-            res.WriteInt32(100102);         //BASE ID
+            res.WriteUInt64((ulong)(10001 + i));      //SPAWN ID
+            res.WriteInt32(100101 + i);         //BASE ID
             res.WriteByte(1);         //QUANTITY
-            res.WriteUInt32(1); //STATUSES
+            res.WriteUInt32((uint)2); //STATUSES
             res.WriteFixedString("", 0x10); //UNKNOWN - ITEM TYPE?
             res.WriteByte(0);      // STORAGE ZONE
             res.WriteByte(0);            //BAG
-            res.WriteInt16(2);    //SLOT
+            res.WriteInt16((short)i);    //SLOT
             res.WriteInt32(0);           //bit mask. This indicates where to put items. PREV EQUIP SLOT
-            res.WriteInt32(99); //spirit eq mask??? PREV DURABILITY
-            res.WriteInt32(100101);  //new
-            res.WriteByte(0);   //ENHANCEMENT LEVEL?
+            res.WriteInt32(0); //spirit eq mask??? PREV DURABILITY
+            res.WriteInt32(0);  //new
+            res.WriteByte((byte)i);   //ENHANCEMENT LEVEL?
             res.WriteByte(0); //SPECIAL FORGE LEVEL?
             res.WriteCString("MAYBE LORE"); // unknown
             res.WriteInt16(0); //PHYSICAL
             res.WriteInt16(0); //MAGICAL
-            res.WriteInt32(100); //MAX DURABILITY
+            res.WriteInt32(0); //MAX DURABILITY
             res.WriteByte(5); //HARDNESS
-            res.WriteInt32(100101); //UNKNOWN
+            res.WriteInt32(0); //UNKNOWN
 
             const int MAX_WHATEVER_SLOTS = 2;
             int numEntries = 2;
@@ -71,128 +72,128 @@ namespace Necromancy.Server.Chat.Command.Commands
                 res.WriteInt32(0);                       //maybe gem item 2 id for diamon 2 gem combine 
             }
 
-            res.WriteInt32(100101);
+            res.WriteInt32(0);
             //res.WriteInt32((0b1 >> _inventoryItem.BagSlotIndex));
             res.WriteInt64(0);//new
-            res.WriteInt16(0xff); //0 = green (in shop for sale)  0xFF = normal /*item.ShopStatus*/
+            res.WriteInt16(0xFF); //0 = green (in shop for sale)  0xFF = normal /*item.ShopStatus*/
             res.WriteInt32(1); //ENCHANT ID
             res.WriteInt16(1); //GP
 
             numEntries = 5; //new
             res.WriteInt32(numEntries); // less than or equal to 5
-            for (int i = 0; i < numEntries; i++)
+            for (int j = 0; j < numEntries; j++)
             {
-                res.WriteInt32(100101);//new
-                res.WriteByte(0);//new
-                res.WriteByte(0);//new
-                res.WriteInt16(0);//new
-                res.WriteInt16(0);//new
+                res.WriteInt32(9);//new
+                res.WriteByte(9);//new
+                res.WriteByte(9);//new
+                res.WriteInt16(9);//new
+                res.WriteInt16(9);//new
             }
 
-            res.WriteInt64(100101);//new
-            res.WriteInt16(2);//new
-            res.WriteInt16(3);//new
-            res.WriteInt16(4);//new
-            res.WriteInt16(5);//new
-            res.WriteInt16(6);//new
-            res.WriteInt16(7);//new
-            res.WriteInt16(8);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
+            res.WriteInt64(1);//new
+            res.WriteInt16((short)1);//new
+            res.WriteInt16((short)1);//new
+            res.WriteInt16((short)1);//new
+            res.WriteInt16((short)1);//new
+            res.WriteInt16((short)1);//new
+            res.WriteInt16((short)1);//new
+            res.WriteInt16((short)1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
 
-            for (int i = 0; i < numEntries; i++)
+            for (int j = 0; j < numEntries; j++)
             {
-                res.WriteInt32(100101);//new
-                res.WriteByte(20);//new
-                res.WriteByte(21);//new
-                res.WriteInt16(22);//new
-                res.WriteInt16(23);//new
+                res.WriteInt32(i);//new
+                res.WriteByte((byte)i);//new
+                res.WriteByte((byte)i);//new
+                res.WriteInt16((short)i);//new
+                res.WriteInt16((short)i);//new
             }
 
-            res.WriteInt16(24);//new
-            res.WriteInt16(25);//new
-            res.WriteByte(26);//new
-            res.WriteByte(27);//new
+            res.WriteInt16((short)i);//new
+            res.WriteInt16((short)i);//new
+            res.WriteByte((byte)i);//new
+            res.WriteByte((byte)i);//new
 
-            res.WriteInt64(100101);//new
-            res.WriteInt16(29);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt16(32);//new
-            res.WriteInt16(33);//new
+            res.WriteInt64(i);//new
+            res.WriteInt16((short)i);//new
+            res.WriteInt32(i);//new
+            res.WriteInt32(i);//new
+            res.WriteInt16((short)i);//new
+            res.WriteInt16((short)i);//new
 
-            res.WriteInt32(100101);//new
-            res.WriteInt32(100101);//new
-            res.WriteInt16(36);//new
-            res.WriteInt16(37);//new
+            res.WriteInt32(1);//new
+            res.WriteInt32(1);//new
+            res.WriteInt16(1);//new
+            res.WriteInt16(1);//new
 
             numEntries = 5;
-            for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
             {
-                res.WriteInt16(38);//new
-                res.WriteInt32(100101);//new
-                res.WriteInt32(100101);//new
-                res.WriteInt16(41);//new
-                res.WriteInt16(42);//new
+                res.WriteInt16(1);//new
+                res.WriteInt32(1);//new
+                res.WriteInt32(1);//new
+                res.WriteInt16(1);//new
+                res.WriteInt16(1);//new
             }
 
-            res.WriteInt16(43);//new
+            res.WriteInt16(1);//new
 
             Router.Send(client, (ushort)AreaPacketId.recv_item_instance, res, ServerType.Area);
-
-            //if (command.Length < 1)
-            //{
-            //    responses.Add(ChatResponse.CommandError(client, "To few arguments"));
-            //    return;
-            //}
-
-            //if (!int.TryParse(command[0], out int itemId))
-            //{
-            //    responses.Add(ChatResponse.CommandError(client, $"Invalid Number: {command[0]}"));
-            //    return;
-            //}
-
-            //if (!Server.Items.ContainsKey(itemId))
-            //{
-            //    responses.Add(ChatResponse.CommandError(client, $"ItemId: '{itemId}' does not exist"));
-            //    return;
-            //}
-
-            //int[] DonkeyItems = new int[] {100101,100102,100103,100104,100105,100106,100107,100108,100109,100110,100111,100112,100113,100114,100115,100116,100181,100191,100192,100193,100201,100202,100203,100204,100205,100206,100207,100208,100209,100210,100211,100212,100213,100214,100301,100302,100303,100304,100305,100306,100307,100308,100401,100402,100403,100404,100405,100406,100407,100408};
-            //int numItems = DonkeyItems.Count();
-
-            //for (int i = 0; i < numItems*5; i++)
-            //{
-            //    Item item = Server.Items[100101];
-            //    // Create InventoryItem
-            //    InventoryItem inventoryItem = new InventoryItem();
-            //    inventoryItem.Id = item.Id + i;
-            //    inventoryItem.ItemId = item.Id;
-            //    inventoryItem.Quantity = 1;
-            //    inventoryItem.CurrentDurability = item.Durability;
-            //    inventoryItem.CharacterId = client.Character.Id;
-            //    inventoryItem.CurrentEquipmentSlotType = EquipmentSlotType.NONE;
-            //    inventoryItem.State = 0;
-            //    inventoryItem.StorageType = (int)BagType.AvatarInventory;
-            //    client.Character.Inventory.AddAvatarItem(inventoryItem);
-            //    inventoryItem.Item.ItemType = (ItemType)(i);
-            //    inventoryItem.Item.Name = $"ItemType + {i}"; 
-
-            //    RecvItemInstance recvItemInstance = new RecvItemInstance(inventoryItem, client);
-            //Router.Send(recvItemInstance, client);
-            //    RecvItemInstanceUnidentified recvItemInstanceUnidentified = new RecvItemInstanceUnidentified(inventoryItem, client);
-            //    Router.Send(recvItemInstanceUnidentified, client);
-            //    responses.Add(ChatResponse.CommandInfo(client, $"item {item.Id} in slot {inventoryItem.BagSlotIndex} in bag {inventoryItem.BagId}"));
-            //}
-        
         }
+        //if (command.Length < 1)
+        //{
+        //    responses.Add(ChatResponse.CommandError(client, "To few arguments"));
+        //    return;
+        //}
+
+        //if (!int.TryParse(command[0], out int itemId))
+        //{
+        //    responses.Add(ChatResponse.CommandError(client, $"Invalid Number: {command[0]}"));
+        //    return;
+        //}
+
+        //if (!Server.Items.ContainsKey(itemId))
+        //{
+        //    responses.Add(ChatResponse.CommandError(client, $"ItemId: '{itemId}' does not exist"));
+        //    return;
+        //}
+
+        //int[] DonkeyItems = new int[] {100101,100102,100103,100104,100105,100106,100107,100108,100109,100110,100111,100112,100113,100114,100115,100116,100181,100191,100192,100193,100201,100202,100203,100204,100205,100206,100207,100208,100209,100210,100211,100212,100213,100214,100301,100302,100303,100304,100305,100306,100307,100308,100401,100402,100403,100404,100405,100406,100407,100408};
+        //int numItems = DonkeyItems.Count();
+
+        //for (int i = 0; i < numItems*5; i++)
+        //{
+        //    Item item = Server.Items[100101];
+        //    // Create InventoryItem
+        //    InventoryItem inventoryItem = new InventoryItem();
+        //    inventoryItem.Id = item.Id + i;
+        //    inventoryItem.ItemId = item.Id;
+        //    inventoryItem.Quantity = 1;
+        //    inventoryItem.CurrentDurability = item.Durability;
+        //    inventoryItem.CharacterId = client.Character.Id;
+        //    inventoryItem.CurrentEquipmentSlotType = EquipmentSlotType.NONE;
+        //    inventoryItem.State = 0;
+        //    inventoryItem.StorageType = (int)BagType.AvatarInventory;
+        //    client.Character.Inventory.AddAvatarItem(inventoryItem);
+        //    inventoryItem.Item.ItemType = (ItemType)(i);
+        //    inventoryItem.Item.Name = $"ItemType + {i}"; 
+
+        //    RecvItemInstance recvItemInstance = new RecvItemInstance(inventoryItem, client);
+        //Router.Send(recvItemInstance, client);
+        //    RecvItemInstanceUnidentified recvItemInstanceUnidentified = new RecvItemInstanceUnidentified(inventoryItem, client);
+        //    Router.Send(recvItemInstanceUnidentified, client);
+        //    responses.Add(ChatResponse.CommandInfo(client, $"item {item.Id} in slot {inventoryItem.BagSlotIndex} in bag {inventoryItem.BagId}"));
+        //}
+
+    }
     }
 }
