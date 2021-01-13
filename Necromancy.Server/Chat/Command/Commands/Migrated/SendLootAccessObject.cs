b7b7e50554fcea340910023255e2c1c1069d7325
Raezaiel <istari40@hotmail.com>
2021-01-12 20:00:57 -0500

    test

--- Necromancy.Server/Chat/Command/Commands/Migrated/SendLootAccessObject.cs
+++ Necromancy.Server/Chat/Command/Commands/Migrated/SendLootAccessObject.cs
@@ -17,13 +17,51 @@ namespace Necromancy.Server.Chat.Command.Commands
             List<ChatResponse> responses)
         {
             IBuffer res = BufferProvider.Provide();
-            res.WriteInt64(10003);
-            res.WriteByte(0);
-            res.WriteByte(0);
-            res.WriteInt16(2);            
-            Router.Send(client, (ushort)AreaPacketId.recv_item_update_place, res, ServerType.Area);
+            res.WriteInt64(100000000000);
+            res.WriteByte(0);//bool
+            Router.Send(client, (ushort)AreaPacketId.recv_self_exp_notify, res, ServerType.Area);
 
+            res = BufferProvider.Provide();
+            res.WriteInt64(10000);
+            //Router.Send(client, (ushort)AreaPacketId.recv_self_money_notify, res, ServerType.Area);
 
+             res = BufferProvider.Provide();
+            res.WriteInt32(1);
+            //Router.Send(client, (ushort)AreaPacketId.recv_self_soul_point_notify, res, ServerType.Area);
+
+            res = BufferProvider.Provide();
+            res.WriteInt32(100101);
+            res.WriteCString("Derp");
+            res.WriteByte(1);
+            //Router.Send(client, (ushort)MsgPacketId.recv_party_notify_get_money, res, ServerType.Area);
+
+            res = BufferProvider.Provide();
+            res.WriteInt32(5);
+            res.WriteInt64(500);
+            //Router.Send(client, (ushort)MsgPacketId.recv_party_notify_get_money, res, ServerType.Area);
+
+             res = BufferProvider.Provide();
+            res.WriteInt16(1);  //item row id
+            res.WriteInt64(10001); //item instance ID
+            res.WriteInt64(10001); //item sale price
+            //Router.Send(client, (ushort)AreaPacketId.recv_0x94B9, res, ServerType.Area);
+
+            res = BufferProvider.Provide();
+            int numEntries = 0xA;
+            res.WriteInt32(numEntries);
+            for (int i = 0; i < numEntries; i++)
+            {
+                res.WriteInt16(1);
+                res.WriteInt64(10001);
+                res.WriteFixedString("test", 0xC1);
+            }
+            //Router.Send(client, (ushort)AreaPacketId.recv_0xBA61, res, ServerType.Area);
+
+            res = BufferProvider.Provide();
+            res.WriteInt32(100101);
+            res.WriteCString("ToBeFound");
+            res.WriteByte(1);
+            //Router.Send(client, (ushort)AreaPacketId.recv_party_notify_get_item, res, ServerType.Area);
             //IBuffer res = BufferProvider.Provide();
             //res.WriteInt32(-10);
             /*
@@ -33,7 +71,7 @@ namespace Necromancy.Server.Chat.Command.Commands
                 LOOT	-1500	No permission to loot
             */
 
-            
+
         }
 
         public override AccountStateType AccountState => AccountStateType.User;
--- Necromancy.Server/Packet/Area/send_event_treasurebox_select.cs
+++ Necromancy.Server/Packet/Area/send_event_treasurebox_select.cs
@@ -16,7 +16,19 @@ namespace Necromancy.Server.Packet.Area
 
         public override void Handle(NecClient client, NecPacket packet)
         {
+
+            byte fromStoreType = packet.Data.ReadByte();
+            byte fromBagId = packet.Data.ReadByte();
+            short fromSlot = packet.Data.ReadInt16();
+
             IBuffer res = BufferProvider.Provide();
+            res.WriteInt64(10003);
+            res.WriteByte(0);
+            res.WriteByte(0);
+            res.WriteInt16(2);
+            Router.Send(client, (ushort)AreaPacketId.recv_item_update_place, res, ServerType.Area);
+
+            res = BufferProvider.Provide();
             res.WriteInt32(0);
             Router.Send(client.Map, (ushort) AreaPacketId.recv_event_treasurebox_select_r, res, ServerType.Area);
         }
