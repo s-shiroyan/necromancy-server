e7ac7e3b540384ac2a15dad2cba79108dcc5b11a
Raezaiel <istari40@hotmail.com>
2021-01-17 13:41:36 -0500

    added hash code and equals overrides

--- Necromancy.Server/Systems/Item/ItemLocation.cs
+++ Necromancy.Server/Systems/Item/ItemLocation.cs
@@ -11,18 +11,29 @@ namespace Necromancy.Server.Systems.Item
             Zone = zone;
             Bag = bag;
             Slot = slot;
+            _hashcode = ((int) zone << 24) + (bag << 16) + slot;
         }
 
         public ItemZone Zone { get; }
         public byte Bag { get; }
         public short Slot { get; }
 
+        private readonly int _hashcode;
+
         public bool Equals(ItemLocation other)
         {
-            if(other.Zone != Zone) return false;
-            if(other.Bag != Bag) return false;
-            if(other.Slot != Slot) return false;
+            if(other.Zone != Zone)  return false;
+            if(other.Bag  != Bag)   return false;
+            if(other.Slot != Slot)  return false;
             return true;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is ItemLocation && Equals((ItemLocation) obj);
+        }
+
+        public override int GetHashCode() { return _hashcode; }
     }
 }
