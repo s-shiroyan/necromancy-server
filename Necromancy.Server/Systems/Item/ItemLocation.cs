3257f9afdeb4c7cff6fffc9651596e96c7da2299
Raezaiel <istari40@hotmail.com>
2021-01-17 09:27:48 -0500

    add struct equatable for no boxing compares

--- Necromancy.Server/Systems/Item/ItemLocation.cs
+++ Necromancy.Server/Systems/Item/ItemLocation.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Necromancy.Server.Systems.Item
 {
-    public readonly struct ItemLocation
+    public readonly struct ItemLocation : IEquatable<ItemLocation>
     {
         public ItemLocation(ItemZone zone, byte bag, short slot)
         {
@@ -16,5 +16,13 @@ namespace Necromancy.Server.Systems.Item
         public ItemZone Zone { get; }
         public byte Bag { get; }
         public short Slot { get; }
+
+        public bool Equals(ItemLocation other)
+        {
+            if(other.Zone != Zone) return false;
+            if(other.Bag != Bag) return false;
+            if(other.Slot != Slot) return false;
+            return true;
+        }
     }
 }
